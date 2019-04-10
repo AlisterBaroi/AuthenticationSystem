786725f9e7230fef21e4f8b52b6de038dbaaa344
Alister Animesh Baroi <44337842+AlisterBaroi@users.noreply.github.com>
2019-04-10 18:30:54 +0600

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -4,11 +4,14 @@ COPYRIGHT ALISTER ANIMESH BAROI -->
 	include('header.php');
 	if (!isset($_SESSION['user'])) {
 		header("location: login.php");
+	}else {
+		$uname = $_SESSION['user'];
 	}
 	
 ?>
 <div class="p-5 bg-danger">
-	<h1 class="text-warning text-center p-5 font-weight-bold display-1">You Are Logged In</h1>
+	<h1 class="text-warning text-center p-5 font-weight-bold display-1">Welcome "<?php echo ($uname); ?>"</h1>
+	<h1 class="text-warning text-center font-weight-bold display-4">You Are Logged In</h1>
 
 </div>
 <img src="" alt="" class="slider">
